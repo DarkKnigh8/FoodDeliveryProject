ec867b7abd57bc2213c4db08e425195dddaaad10
user <mihiduniabeyratne@gmial.com>
2025-04-21 01:55:30 -0700

    mihi

--- backend/order_service/src/middleware/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-const errorMiddleware = (err, req, res, next) => {
-    console.error("❌ Error:", err.stack);
-    res.status(500).json({ message: err.message || "Internal Server Error" });
-  };
-  
-  module.exports = errorMiddleware;
-  
