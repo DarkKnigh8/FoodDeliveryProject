50efc3f849aa4c6e07244ebb47918bd53cad8e2c
user <mihiduniabeyratne@gmial.com>
2025-04-24 17:53:52 -0700

    mihi

--- backend/delivery_service/src/routes/driverRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerDriver, updateDriverLocation } = require('../controllers/driverController');
-
-const router = express.Router();
-
-router.post('/register', registerDriver);
-router.put('/:driverId/location', updateDriverLocation);
-
-module.exports = router;
